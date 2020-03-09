2c377c77421cc62addda0ecb3d5a479f2b3e6ebb
Nickey Kolev <nickey.kolev@gmail.com>
2020-03-09 18:15:19 +0200

    init

---
+++ Services/ProfileMS/Controllers/ProfileController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProfileMS.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProfileController : ControllerBase
+    {
+        private readonly ILogger<ProfileController> _logger;
+
+        private IEnumerable<Profile> _profiles;
+
+        public ProfileController(ILogger<ProfileController> logger)
+        {
+            _logger = logger;
+            _profiles = new List<Profile>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                _profiles.Append(new Profile {
+                    Id = i,
+                    Name = " User" + i.ToString() 
+                });
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<Profile> GetAll()
+        {
+            return _profiles;
+        }
+    }
+}
---
+++ Services/ProfileMS/Profile.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ProfileMS
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
